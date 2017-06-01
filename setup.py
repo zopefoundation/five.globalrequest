886d92ec95c1ac4715cfe4b513ec2a8c1aaaec54
Michael Howitz <mh@gocept.com>
2017-06-01 16:35:27 +0200

    Preparing release 99.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='five.globalrequest',
-    version='99.0dev',
+    version='99.0',
     description="DEPRECATED: Zope integration for zope.globalrequest",
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.rst").read()),
